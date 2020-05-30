159209bf3a502b8e3d2ecc74e7f0f4b7f09c8df8
mbezruchko <xasener71@gmail.com>
2020-05-30 13:15:03 +0300

    hotfix text & slider

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,22 @@
 window.onload = () => {
   let slider = document.getElementsByClassName('apartment_gallery_content')[0]
   let last_slide = slider.scrollWidth - slider.clientWidth
+  let header_arrows = document.getElementsByClassName('apartment_gallery_switch')
+
+  seconds = (sec) => {
+    return sec * 1000
+  }
+
+  lockArrow = () => {
+    Object.entries(header_arrows).forEach(function(element) {
+      header_arrows[element[0]].style.pointerEvents='none'
+    });
+    setTimeout(() => {
+      Object.entries(header_arrows).forEach(function(element) {
+        header_arrows[element[0]].style.pointerEvents='auto'
+      });
+    }, seconds(0.5));
+  }
 
   let autoSlide = setInterval(() => {
     if (slider.scrollLeft === last_slide) {
@@ -9,13 +25,16 @@ window.onload = () => {
     else {
       slider.scrollLeft += slider.clientWidth
     }
-  }, 4000)
+  }, seconds(4))
 
   nextSlide = () =>  {
+    console.log(header_arrows)
+    lockArrow()
     slider.scrollLeft += slider.clientWidth
     clearInterval(autoSlide)
   }
   previousSlide = () => {
+    lockArrow()
     slider.scrollLeft -= slider.clientWidth
     clearInterval(autoSlide)
   }
